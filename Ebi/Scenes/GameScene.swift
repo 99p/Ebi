e804ddb27a1371e63cdd1bebc94c8a106ad2aafd
99p <poccori@gmail.com>
2022-07-18 16:18:48 +0900

    1st commit

---
+++ Ebi/Scenes/GameScene.swift
@@ -0,0 +1,16 @@
+//
+//  GameScene.swift
+//  Ebi
+//
+//  Created by macboy on 2022/07/18.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    override func didMove(to view: SKView) {
+        self.backgroundColor = .purple
+    }
+    
+}
