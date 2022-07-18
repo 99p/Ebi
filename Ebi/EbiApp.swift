273097683a7b65750e9255f404cf793fb8bd1eda
99p <poccori@gmail.com>
2022-07-18 16:02:05 +0900

    Initial Commit

---
+++ Ebi/EbiApp.swift
@@ -0,0 +1,17 @@
+//
+//  EbiApp.swift
+//  Ebi
+//
+//  Created by macboy on 2022/07/18.
+//
+
+import SwiftUI
+
+@main
+struct EbiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
