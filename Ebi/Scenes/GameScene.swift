950ca901a4c4cfcd78f9530e64efaae30cdafa53
99p <poccori@gmail.com>
2022-07-26 21:54:38 +0900

    Add land and ceiling

--- Ebi/Scenes/GameScene.swift
+++ Ebi/Scenes/GameScene.swift
@@ -10,12 +10,25 @@ import SpriteKit
 
 class GameScene: SKScene {
     
+    struct ColliderType {
+        static let Player: UInt32 = (1 << 0)
+        static let World:  UInt32 = (1 << 1)
+        static let Coral:  UInt32 = (1 << 2)
+        static let Score:  UInt32 = (1 << 3)
+        static let None:   UInt32 = (1 << 4)
+    }
+    
     var baseNode: SKNode!
     var coralNode: SKNode!
     var yscale: CGFloat = 0.0
     
+    override func sceneDidLoad() {
+        self.scaleMode = .resizeFill
+//        self.anchorPoint = CGPoint(x: 0.5,
+//                                   y: 0.5)
+    }
+    
     override func didMove(to view: SKView) {
-//        self.backgroundColor = .purple
         // 全ノードの親となるノードを生成
         baseNode = SKNode()
         baseNode.speed = 1.0
@@ -28,15 +41,8 @@ class GameScene: SKScene {
         // 背景画像を構築
         self.setupBackgroundSea()
         self.setupBackgroundRock()
+        self.setupCeilingAndLand()
     }
-    
-//    func simpp(){
-//        let tex = SKTexture(imageNamed: "background")
-//        let sprite = SKSpriteNode(texture: tex)
-//        sprite.setScale(self.frame.size.width/tex.size().width)
-//        sprite.position = CGPoint(x: sprite.size.width/2, y: self.frame.size.height/2.0)
-//        baseNode.addChild(sprite)
-//    }
     
     func setupBackgroundSea(){
         // 背景画像を読み込む
@@ -50,7 +56,7 @@ class GameScene: SKScene {
         // アニメーションを作成
         let moveAnim = SKAction.moveBy(x: -self.frame.size.width,
                                        y: 0.0,
-                                       duration: TimeInterval(self.frame.size.width * 18))
+                                       duration: TimeInterval(self.frame.size.width / 18))
         let resetAnim = SKAction.moveBy(x: self.frame.size.width,
                                         y: 0.0,
                                         duration: 0.0)
@@ -81,7 +87,7 @@ class GameScene: SKScene {
         // アニメーションを作成
         let moveUnderAnim = SKAction.moveBy(x: -self.frame.size.width,
                                             y: 0.0,
-                                            duration: TimeInterval(self.frame.size.width * 5.0))
+                                            duration: TimeInterval(self.frame.size.width / 30.0))
         let resetUnderAnim = SKAction.moveBy(x: self.frame.size.width,
                                              y: 0.0,
                                              duration: 0.0)
@@ -95,7 +101,7 @@ class GameScene: SKScene {
             let scale  = self.frame.size.width  / sprite.size.width
             sprite.setScale(scale)
             sprite.yScale = yscale
-            sprite.position = CGPoint(x: i * sprite.size.width, y: sprite.size.height / 4.0)
+            sprite.position = CGPoint(x: i * sprite.size.width, y: sprite.size.height / 2.0)
             sprite.run(repeatForeverUnderAnim)
             baseNode.addChild(sprite)
         }
@@ -105,7 +111,7 @@ class GameScene: SKScene {
         
         let moveAboveAnim = SKAction.moveBy(x: -self.frame.size.width,
                                             y: 0.0,
-                                            duration: TimeInterval(self.frame.size.width * 5.0))
+                                            duration: TimeInterval(self.frame.size.width / 30.0))
         let resetAboveAnim = SKAction.moveBy(x: self.frame.size.width,
                                              y: 0.0,
                                              duration: 0.0)
@@ -119,10 +125,75 @@ class GameScene: SKScene {
             sprite.setScale(self.frame.size.width/sprite.size.width)
             sprite.yScale = yscale
             sprite.position = CGPoint(x: i * sprite.size.width,
-                                      y: self.frame.size.height - (sprite.size.height / 100.0))
+                                      y: self.frame.size.height - (sprite.size.height / 2.0))
             sprite.run(repeatForeverAboveAnim)
             baseNode.addChild(sprite)
         }
+        
+    }
+    
+    func setupCeilingAndLand(){
+        // load images
+        let land = SKTexture(imageNamed: "land")
+        land.filteringMode = .nearest
+        
+        // neednumber
+        var needNumber = 2.0 + ceil(self.frame.size.width / land.size().width)
+        
+        // create Animation
+        let moveLandAnim = SKAction.moveBy(x: -land.size().width,
+                                           y: 0.0,
+                                           duration: TimeInterval(land.size().width / 80.0))
+        let resetLandAnim = SKAction.moveBy(x: land.size().width,
+                                            y: 0.0,
+                                            duration: 0.0)
+        let sequenceLandAnim = SKAction.sequence([ moveLandAnim, resetLandAnim ])
+        let repeatForeverLandAnim = SKAction.repeatForever(sequenceLandAnim)
+        
+        // set images and Animations
+        for num in 0 ..< Int(needNumber){
+            let i: CGFloat = CGFloat(num)
+            let sprite = SKSpriteNode(texture: land)
+            sprite.position = CGPoint(x: i * sprite.size.width,
+                                      y: sprite.size.height / 2.0)
+            sprite.physicsBody = SKPhysicsBody(texture: land,
+                                               size:land.size())
+            sprite.physicsBody?.isDynamic = false
+            sprite.physicsBody?.categoryBitMask = ColliderType.World
+            sprite.run(repeatForeverLandAnim)
+            baseNode.addChild(sprite)
+        }
+        // load images
+        let ceiling = SKTexture(imageNamed: "ceiling")
+        ceiling.filteringMode = .nearest
+        
+        // neednumber
+        needNumber = 2.0 + ceil(self.frame.size.width / ceiling.size().width)
+        
+        // create Animation
+        let moveCeilingAnim = SKAction.moveBy(x: -ceiling.size().width,
+                                           y: 0.0,
+                                           duration: TimeInterval(ceiling.size().width / 80.0))
+        let resetCeilingAnim = SKAction.moveBy(x: ceiling.size().width,
+                                            y: 0.0,
+                                            duration: 0.0)
+        let sequenceCeilingAnim = SKAction.sequence([ moveCeilingAnim, resetCeilingAnim ])
+        let repeatForeverCeilingAnim = SKAction.repeatForever(sequenceCeilingAnim)
+        
+        // set images and Animations
+        for num in 0 ..< Int(needNumber){
+            let i: CGFloat = CGFloat(num)
+            let sprite = SKSpriteNode(texture: ceiling)
+            sprite.position = CGPoint(x: i * sprite.size.width,
+                                      y: self.frame.size.height / 1.05)
+            sprite.physicsBody = SKPhysicsBody(texture: ceiling,
+                                               size:ceiling.size())
+            sprite.physicsBody?.isDynamic = false
+            sprite.physicsBody?.categoryBitMask = ColliderType.World
+            sprite.run(repeatForeverCeilingAnim)
+            baseNode.addChild(sprite)
+        }
+
         
     }
     
